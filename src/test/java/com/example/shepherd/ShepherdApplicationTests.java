2c01c12eb6e8a92cbd2e2b3c29f8d6cacc1860b8
Jack Curtin <jackcurtin94@gmail.com>
2021-08-23 15:54:02 -0500

    first commit

---
+++ src/test/java/com/example/shepherd/ShepherdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shepherd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShepherdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
