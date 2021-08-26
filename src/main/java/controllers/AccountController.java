b4b0dc0f4d82099873a63b9ed699f30c44c7f20a
Jack Curtin <jackcurtin94@gmail.com>
2021-08-26 10:39:52 -0500

    account controller connected to service

--- src/main/java/controllers/AccountController.java
+++ src/main/java/controllers/AccountController.java
@@ -1,8 +1,13 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import services.AccountService;
 
 import java.util.List;
 
@@ -11,11 +16,23 @@ import com.example.shepherd.models.Account;
 @RestController
 @RequestMapping(path = "/api/accounts/")
 public class AccountController {
+    private AccountService accountService;
+
+    @Autowired
+    private void setAccountService (AccountService accountService){
+        this.accountService = accountService;
+    }
 
     @GetMapping
     public List<Account> getAllAccounts(){
         System.out.println("Controller calling getAllAccounts");
-        
+        return accountService.getAllAccounts();
+    }
+
+    @PostMapping("/register")
+    public Account createAccount(@RequestBody Account accountObject){
+        System.out.println("Controller calling createAccount");
+        return accountService.createAccount(accountObject);
     }
     
 }
