afa55846eb85d8325d9c2f77b4e766e29c8ef178
Jack Curtin <jackcurtin94@gmail.com>
2021-08-26 10:07:58 -0500

    created account repo

---
+++ src/main/java/repos/AccountRepo.java
@@ -0,0 +1,9 @@
+package repos;
+
+import com.example.shepherd.models.Account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepo extends JpaRepository<Account, Long> {
+    
+}
