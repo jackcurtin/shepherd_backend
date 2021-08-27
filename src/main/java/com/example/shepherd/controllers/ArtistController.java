2d5bc526f1963a3c19a9f98e8ad776d881331997
Jack Curtin <jackcurtin94@gmail.com>
2021-08-27 13:44:51 -0500

    basic artist controller&service methods implemented

--- src/main/java/com/example/shepherd/controllers/ArtistController.java
+++ src/main/java/com/example/shepherd/controllers/ArtistController.java
@@ -1,11 +1,38 @@
 package com.example.shepherd.controllers;
 
+import java.util.List;
+
+import com.example.shepherd.models.Artist;
+import com.example.shepherd.services.ArtistService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/artists")
 public class ArtistController {
-    
+    private ArtistService artistService;
+
+    @Autowired
+    private void setArtistService(ArtistService artistService){
+        this.artistService = artistService;
+    }
+
+    @GetMapping("/all")
+    public List<Artist> getAllArtists(){
+        System.out.println("Controller calling getAllArtists");
+        return artistService.getAllArtists();
+    }
+
+    @PostMapping("/add")
+    public Artist addArtist(@RequestBody Artist artistObject){
+        System.out.println("Controller calling addArtist");
+        return artistService.addArtist(artistObject);
+    }
+
     
 }
--- src/main/java/com/example/shepherd/services/ArtistService.java
+++ src/main/java/com/example/shepherd/services/ArtistService.java
@@ -1,9 +1,30 @@
 package com.example.shepherd.services;
 
+import java.util.List;
+
+import com.example.shepherd.models.Artist;
+import com.example.shepherd.repos.ArtistRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArtistService {
-    
+    private ArtistRepo artistRepo;
+
+    @Autowired
+    private void setArtistRepo(ArtistRepo artistRepo){
+        this.artistRepo = artistRepo;
+    }
+
+    public List<Artist> getAllArtists(){
+        System.out.println("Service calling getAllArtists");
+        return artistRepo.findAll();
+    }
+
+    public Artist addArtist(Artist artistObject){
+        System.out.println("Service calling addArtist");
+        return artistRepo.save(artistObject);
+    }
     
 }
