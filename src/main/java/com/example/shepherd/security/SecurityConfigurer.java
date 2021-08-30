7718b78258479afff97cf76f04e547b4e505e166
Jack Curtin <jackcurtin94@gmail.com>
2021-08-30 15:06:41 -0500

    changed configurer for testing

--- src/main/java/com/example/shepherd/security/SecurityConfigurer.java
+++ src/main/java/com/example/shepherd/security/SecurityConfigurer.java
@@ -35,7 +35,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/*").permitAll().anyRequest()
+        http.authorizeRequests().antMatchers("/auth/*").permitAll().anyRequest()
             .authenticated().and().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().csrf().disable();
