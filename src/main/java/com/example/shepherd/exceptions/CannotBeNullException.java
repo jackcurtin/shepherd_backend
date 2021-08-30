6342eba122b773ce9b25a7c5258ca2a0cc1eb804
Jack Curtin <jackcurtin94@gmail.com>
2021-08-30 10:33:10 -0500

    added cannot be null exception

---
+++ src/main/java/com/example/shepherd/exceptions/CannotBeNullException.java
@@ -0,0 +1,8 @@
+package com.example.shepherd.exceptions;
+
+public class CannotBeNullException extends RuntimeException{
+    public CannotBeNullException(String message){
+        super(message);
+    }
+    
+}
