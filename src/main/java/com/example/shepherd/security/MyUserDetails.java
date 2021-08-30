d5559ac073d99368d4f6f933679c38a79ba01a07
Jack Curtin <jackcurtin94@gmail.com>
2021-08-30 10:06:47 -0500

    MyUserDetails filled out

--- src/main/java/com/example/shepherd/security/MyUserDetails.java
+++ src/main/java/com/example/shepherd/security/MyUserDetails.java
@@ -1,5 +1,56 @@
 package com.example.shepherd.security;
 
-public class MyUserDetails {
-    
+import java.util.Collection;
+import java.util.HashSet;
+
+import com.example.shepherd.models.Account;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails {
+    private Account account;
+
+    public MyUserDetails(Account account){
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return new HashSet<GrantedAuthority>();
+    }
+
+    @Override
+    public String getPassword(){
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return account.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
 }
