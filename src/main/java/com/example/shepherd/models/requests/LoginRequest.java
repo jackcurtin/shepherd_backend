45d9d1130bb420772cfe3c5268dc22d25e657199
Jack Curtin <jackcurtin94@gmail.com>
2021-08-31 10:01:16 -0500

    created request and response for login

---
+++ src/main/java/com/example/shepherd/models/requests/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.shepherd.models.requests;
+
+public class LoginRequest {
+    private String emailAddress;
+    private String password;
+
+    public String getEmailAddress(){
+        return emailAddress;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    
+}
