0c3e17f9946c168c57521d46ac725c51ddb74984
Jack Curtin <jackcurtin94@gmail.com>
2021-09-01 14:28:58 -0500

    making createRelease take a map of strings

--- src/main/java/com/example/shepherd/services/ReleaseService.java
+++ src/main/java/com/example/shepherd/services/ReleaseService.java
@@ -1,9 +1,12 @@
 package com.example.shepherd.services;
 
+import java.util.Map;
 import java.util.Optional;
 
 import com.example.shepherd.exceptions.InformationExistsException;
 import com.example.shepherd.models.Release;
+import com.example.shepherd.repos.ArtistRepo;
+import com.example.shepherd.repos.LabelRepo;
 import com.example.shepherd.repos.ReleaseRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +15,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReleaseService {
     private ReleaseRepo releaseRepo;
+    private ArtistRepo artistRepo;
+    private LabelRepo labelRepo;
 
     @Autowired
     private void ReleaseRepo(ReleaseRepo releaseRepo){
         this.releaseRepo = releaseRepo;
     }
 
-    public Release createRelease(Release releaseObject){
+    @Autowired
+    private void ArtistRepo(ArtistRepo artistRepo){
+        this.artistRepo = artistRepo;
+    }
+
+    @Autowired
+    private void LabelRepo(LabelRepo labelRepo){
+        this.labelRepo = labelRepo;
+    }
+
+    public Release createRelease(Map <String, String> releaseObject){
         System.out.println("Service calling createRelease");
-        Optional<Release> release = releaseRepo.findByTitleAndArtist(releaseObject.getTitle(), releaseObject.getArtist().getName());
-        if(release.isPresent()){
-            throw new InformationExistsException(release.get().getTitle() + " by " + release.get().getArtist().getName() + "already in database");
+        Optional<Release> releaseOpt = releaseRepo.findByTitleAndArtist(releaseObject.get("title"), releaseObject.get("artist") );
+        if(releaseOpt.isPresent()){
+            throw new InformationExistsException(releaseOpt.get().getTitle() + " by " + releaseOpt.get().getArtist().getName() + "already in database");
         }
         else{
-            return releaseRepo.save(releaseObject);
+            Release release = new Release(releaseObject.get("title"));
+            release.setArtist(artistRepo);
+            release.setLabel();
+            return releaseRepo.save(release);
         }
         
     }
