193aeb3327d6e7face0747a1c62fd393a1607f65
Jack Curtin <jackcurtin94@gmail.com>
2021-09-02 10:46:00 -0500

    updateArtist implemented

--- src/main/java/com/example/shepherd/controllers/ArtistController.java
+++ src/main/java/com/example/shepherd/controllers/ArtistController.java
@@ -1,13 +1,16 @@
 package com.example.shepherd.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import com.example.shepherd.models.Artist;
 import com.example.shepherd.services.ArtistService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +35,12 @@ public class ArtistController {
     public Artist addArtist(@RequestBody Artist artistObject){
         System.out.println("Controller calling addArtist");
         return artistService.addArtist(artistObject);
+    }
+
+    @PutMapping("/{artistId}/update")
+    public Artist updateArtist(@PathVariable Long artistId, @RequestBody Map<String, String> artistObject){
+        System.out.println("Controller calling updateArtist");
+        return artistService.updateArtist(artistId, artistObject);
     }
 
     
--- src/main/java/com/example/shepherd/services/ArtistService.java
+++ src/main/java/com/example/shepherd/services/ArtistService.java
@@ -1,7 +1,10 @@
 package com.example.shepherd.services;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import com.example.shepherd.exceptions.InformationNotFoundException;
 import com.example.shepherd.models.Account;
 import com.example.shepherd.models.Artist;
 import com.example.shepherd.repos.AccountRepo;
@@ -38,6 +41,19 @@ public class ArtistService {
         Account account = accountRepo.findByEmailAddress(userDetails.getUsername()).get();
         artistObject.setAccount(account);
         return artistRepo.save(artistObject);
+    }
+
+    public Artist updateArtist(Long artistId, Map<String, String> artistObject){
+        System.out.println("Service calling updateArtist");
+        Optional<Artist> artistOpt =  artistRepo.findById(artistId);
+        if(artistOpt.isEmpty()){
+            throw new InformationNotFoundException("Artist not found");
+        }
+        else{
+            artistOpt.get().setName(artistObject.get("name"));
+            artistOpt.get().setCity(artistObject.get("city"));
+            return artistRepo.save(artistOpt.get());
+        }
     }
     
 }
